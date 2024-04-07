89f06d3200212a685c3b3a351e3909e38215d5fb
TeryohinAE <saschalada@mail.ru>
2024-04-07 20:32:08 +0600

    Start work with the render. He draw triangle

--- src/Event_system/event_system.cpp
+++ src/Event_system/event_system.cpp
@@ -3,7 +3,7 @@
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int wight, int height) {
     settings.set_x_window_size(wight);
     settings.set_y_window_size(height);
-    glViewport(0, 5, settings.get_x_window_size() * 2, settings.get_y_window_size());
+    glViewport(0, 0, settings.get_x_window_size(), settings.get_y_window_size());
 }
 
 void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode) {
--- src/Render/render.hpp
+++ src/Render/render.hpp
@@ -1,5 +1,37 @@
 #pragma once
 #include <iostream>
-void a() {
-	std::cout << "vdsvsdvs\n";
+#include <chrono>
+#include <thread>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+int limit_frame = 60;
+
+
+
+//надо поставить ограничение кадров в секунду
+void update_screen(GLFWwindow* pWindow,GLuint &shader_program, GLuint &vao) {
+    
+    std::chrono::microseconds frame_time((1000/limit_frame));
+    auto last_frame = std::chrono::steady_clock::now();
+
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        last_frame = std::chrono::steady_clock::now();
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(pWindow);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+        auto end_frame = std::chrono::steady_clock::now();
+        std::this_thread::sleep_for((end_frame - last_frame) - frame_time);
+    }
 }
